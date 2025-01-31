41b3d3de804b7aefc2714e8cc905c07c6ad4bb51
i-am-not-amir <amirhosein138313832@gmail.com>
2025-01-30 23:49:57 -0800

    ساخت عدد تصادفی

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('generate-password',views.random_password)
+    path('generate-password',views.random_password),
+    path('generate-number',views.random_number)
 ]
